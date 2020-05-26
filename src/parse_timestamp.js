bd773f117d9d0c395d13c3c0a5de8afb82a935f9
myyrakle <sssang97@naver.com>
2020-05-26 19:39:59 +0900

    Update parse_timestamp.js

--- src/parse_timestamp.js
+++ src/parse_timestamp.js
@@ -1,5 +1,5 @@
 //타임스탬프를 20XX-XX-XX 형식으로 변환
-function timestampToDate(timestamp, deliminator='-'){
+function timestampToDateString(timestamp, deliminator='-'){
   const date = new Date(timestamp);
   return `${date.getFullYear()}${deliminator}${("00" + (date.getMonth() + 1)).slice(-2)}${deliminator}${("00" + date.getDate()).slice(-2) }`;
 }
