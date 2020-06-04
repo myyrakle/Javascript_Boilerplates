1113fda70b36c0e1284d11c24387a29b4de08e5c
myyrakle <sssang97@naver.com>
2020-06-04 11:06:34 +0900

    Update typecheck.js

--- src/typecheck.js
+++ src/typecheck.js
@@ -1,14 +1,19 @@
 function isString(value)
 {
-  return data instanceof String;
+  return value instanceof String;
 }
 
 function isNumber(value)
 {
-  return data instanceof Number;
+  return value instanceof Number;
 }
 
 function isInteger(value)
 {
-  return Number.isInteger(value);
+  return (value === Number(value)) && value%1 === 0;
+}
+
+function isFloat(value)
+{
+  return (value === Number(value)) && value%1 !== 0;
 }
