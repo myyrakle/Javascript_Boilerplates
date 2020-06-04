67b52a6f5670623c80470ffabf930d8f179dd219
myyrakle <sssang97@naver.com>
2020-06-04 11:13:02 +0900

    Update typecheck.js

--- src/typecheck.js
+++ src/typecheck.js
@@ -17,3 +17,29 @@ function isFloat(value)
 {
   return (value === Number(value)) && value%1 !== 0;
 }
+
+function isBoolean(value)
+{
+  return value === Boolean(value);
+}
+
+function isNull(value)
+{
+  return value === null;
+}
+
+function isUndefined(value)
+{
+  return value === undefined;
+}
+
+function isArray(value)
+{
+  Array.isArray(value);
+}
+
+function isObject(value)
+{
+  return value === Object(value);
+}
+
