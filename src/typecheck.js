e464d3e0c3e8ab3a4daf2d7a839b195f35d994e2
myyrakle <sssang97@naver.com>
2020-06-04 10:57:26 +0900

    Create typecheck.js

---
+++ src/typecheck.js
@@ -0,0 +1,14 @@
+function isString(value)
+{
+  return data instanceof String;
+}
+
+function isNumber(value)
+{
+  return data instanceof Number;
+}
+
+function isInteger(value)
+{
+  return Number.isInteger(value);
+}
