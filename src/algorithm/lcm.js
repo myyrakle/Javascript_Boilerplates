abcfb6ed207f42457e1a20d4e4368011923c6ba3
myyrakle <sssang97@naver.com>
2020-11-06 07:59:21 +0900

    Update lcm.js

--- src/algorithm/lcm.js
+++ src/algorithm/lcm.js
@@ -1,13 +1,13 @@
 function lcm(n1, n2) {
-  function _gcd(a, b){
-    if (!b) {
-      return a;
+    function _gcd(a, b){
+        if (!b) {
+            return a;
+        }
+
+        return _gcd(b, a % b);
     }
 
-    return _gcd(b, a % b);
-  }
+    const gcdValue = _gcd(n1, n2);
 
-  const gcdValue = _gcd(n1, n2);
-
-  return (n1 * n2) / gcdValue;
+    return (n1 * n2) / gcdValue;
 }
