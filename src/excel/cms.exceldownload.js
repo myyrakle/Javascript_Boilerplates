045325de7f892f00f43903f85bc1cbabcbae3d61
myyrakle <sssang97@naver.com>
2020-11-12 09:13:51 +0900

    Update cms.exceldownload.js

--- src/excel/cms.exceldownload.js
+++ src/excel/cms.exceldownload.js
@@ -1,6 +1,10 @@
 const {Workbook} = require('exceljs');
 
-// columns [{header:'열 이름', key:'값의 실제 키 이름', width:'가로폭'}]
+// 엑셀 파일을 생성해 전송합니다.
+//
+// response = express 리스폰스 객체
+// list = DB에서 읽어온 값 그대로
+// columns = 생성 형태. [{header:'맨 위에 열 이름', key:'DB select값의 이름', width:'가로폭(평균 10 정도 적당)'}]
 async function sendCMSExcel(response, list, columns)
 {
   const workbook = new Workbook();
