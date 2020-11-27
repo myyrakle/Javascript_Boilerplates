0ab7407a49b13d2f76b6b505bc61acd58ba5887a
myyrakle <sssang97@naver.com>
2020-11-27 13:28:20 +0900

    Update compareSemver.js

--- src/string/compareSemver.js
+++ src/string/compareSemver.js
@@ -1,25 +1,21 @@
 // 왼쪽이 작으면 -1
 // 같으면 0
 // 오른쪽이 크면 1
-function compareSemver(lhs, rhs)
-{
+function compareSemver(lhs, rhs) {
     const l = lhs.split('.').map(Number);
     const r = rhs.split('.').map(Number);
 
     const len = l.length > r.length ? l.length : r.length;
 
-    for(let i = 0; i<len; i++)
-    {
+    for(let i = 0; i<len; i++) {
         let lversion = l[i] || 0;
         let rversion = r[i] || 0;
 
-        if(lversion < rversion) 
-        {
-ㅤ           return -1;
+        if(lversion < rversion) {
+            return -1;
         }
-        if(lversion > rversion) 
-        {
-ㅤ           return 1;
+        if(lversion > rversion) {
+            return 1;
         }
     }
     return 0;
